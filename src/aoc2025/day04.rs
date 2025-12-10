c8c76e998a3eda9b51a94d4aecdbf9f6de05f8b6
chrisvgt <30186211+chrisvgt@users.noreply.github.com>
2025-12-10 23:28:40 +0100

    feat(aoc2025): add day04 solution, part2 missing

---
+++ src/aoc2025/day04.rs
@@ -0,0 +1,133 @@
+use array2d::Array2D;
+use std::string::*;
+
+fn is_accessible(array2d: &Array2D<char>, row: usize, column: usize) -> bool {
+    let mut count = 0;
+
+    let rows = array2d.row_len();
+    let columns = array2d.column_len();
+
+    let has_top = row > 0;
+    let has_left = column > 0;
+
+    let has_bottom = row + 1 < rows;
+    let has_right = column + 1 < columns;
+
+    if has_left && array2d.get(row, column - 1) == Some(&'@') {
+        count += 1;
+    }
+
+    if has_right && array2d.get(row, column + 1) == Some(&'@') {
+        count += 1;
+    }
+
+    if has_top && array2d.get(row - 1, column) == Some(&'@') {
+        count += 1;
+    }
+
+    if has_bottom && array2d.get(row + 1, column) == Some(&'@') {
+        count += 1;
+    }
+
+    if has_top && has_left && array2d.get(row - 1, column - 1) == Some(&'@') {
+        count += 1;
+    }
+
+    if has_top && has_right && array2d.get(row - 1, column + 1) == Some(&'@') {
+        count += 1;
+    }
+
+    if has_bottom && has_left && array2d.get(row + 1, column - 1) == Some(&'@') {
+        count += 1;
+    }
+
+    if has_bottom && has_right && array2d.get(row + 1, column + 1) == Some(&'@') {
+        count += 1;
+    }
+    count < 4
+}
+
+pub fn part1(input: &[String]) -> u64 {
+    let rows = input.len();
+    let columns = input[0].len();
+
+    if rows != columns {
+        return 0;
+    }
+
+    let data: Vec<char> = input.iter().flat_map(|s| s.chars()).collect();
+
+    let array2d =
+        Array2D::from_column_major(&data, rows, columns).expect("failed to create array2D");
+
+    let mut count = 0;
+
+    for i in 0..columns {
+        for j in 0..rows {
+            if array2d.get(j, i) == Some(&'@') {
+                if is_accessible(&array2d, j, i) {
+                    count += 1;
+                    // print!("X")
+                } else {
+                    // print!("@")
+                }
+            } else {
+                // print!(".")
+            }
+        }
+        // println!()
+    }
+    count
+}
+
+#[allow(unused)]
+pub fn part2(input: &[String]) -> u64 {
+    0
+}
+
+#[allow(unused)]
+pub fn solve(input: &[String]) {
+    let now = std::time::Instant::now();
+    let part1 = part1(input);
+    let part2 = part2(input);
+    let elapsed = now.elapsed();
+
+    println!("--- Day 4: Printing Department ---");
+    println!("Part1: {}", part1);
+    println!("Part2: {}", part2);
+    println!("Elapsed: {:.2?}", elapsed);
+}
+
+#[test]
+fn test1() {
+    let v = vec![
+        "..@@.@@@@.".to_string(),
+        "@@@.@.@.@@".to_string(),
+        "@@@@@.@.@@".to_string(),
+        "@.@@@@..@.".to_string(),
+        "@@.@@@@.@@".to_string(),
+        ".@@@@@@@.@".to_string(),
+        ".@.@.@.@@@".to_string(),
+        "@.@@@.@@@@".to_string(),
+        ".@@@@@@@@.".to_string(),
+        "@.@.@@@.@.".to_string(),
+    ];
+    assert_eq!(part1(&v), 13);
+}
+
+#[test]
+fn test2() {
+    let v = vec![
+        "..@@.@@@@.".to_string(),
+        "@@@.@.@.@@".to_string(),
+        "@@@@@.@.@@".to_string(),
+        "@.@@@@..@.".to_string(),
+        "@@.@@@@.@@".to_string(),
+        ".@@@@@@@.@".to_string(),
+        ".@.@.@.@@@".to_string(),
+        "@.@@@.@@@@".to_string(),
+        ".@@@@@@@@.".to_string(),
+        "@.@.@@@.@.".to_string(),
+    ];
+    assert_eq!(part2(&v), 43);
+}
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ fn main() {
     /* Advent of Code 2025 */
     // aoc2025::day01::solve(&utils::io::read_lines("./input/aoc2025/day01.txt"));
     // aoc2025::day02::solve(&utils::io::read_string("./input/aoc2025/day02.txt"));
-    aoc2025::day03::solve(&utils::io::read_lines("./input/aoc2025/day03.txt"));
+    // aoc2025::day03::solve(&utils::io::read_lines("./input/aoc2025/day03.txt"));
+    aoc2025::day04::solve(&utils::io::read_lines("./input/aoc2025/day04.txt"));
 }
