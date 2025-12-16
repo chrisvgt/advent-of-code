d6a3dbac1eb61891149d5de80fcd5cebf2b8ca18
chrisvgt <30186211+chrisvgt@users.noreply.github.com>
2025-12-16 10:21:20 +0100

    chore: debug print macros, allow unused

--- src/utils/dbg.rs
+++ src/utils/dbg.rs
@@ -1,15 +1,19 @@
+#[allow(unused)]
 macro_rules! dbgprint {
     ($($rest:tt)*) => {
         #[cfg(debug_assertions)]
         std::print!($($rest)*)
     }
 }
+#[allow(unused)]
 pub(crate) use dbgprint;
 
+#[allow(unused)]
 macro_rules! dbgprintln {
     ($($rest:tt)*) => {
         #[cfg(debug_assertions)]
         std::println!($($rest)*)
     }
 }
+#[allow(unused)]
 pub(crate) use dbgprintln;
